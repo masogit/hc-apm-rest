6a1b8cbe0ac3224f84f9ff7480ede71cd043a095
Man, Shuo <shuo.man@gmail.com>
2017-02-26 23:47:36 +0800

    Fetch https remote data

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var httpProxy = require('http-proxy');
 var sites = require('./data/site');
 var path = require('path');
+var https = require('https');
 
 // two server for set base root path
 const base = process.env.APM_ROOT || "/apm";
@@ -19,7 +20,7 @@ proxy.on('error', function(e) {
 
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
   console.log('proxyReq');
-  console.log(proxyReq);
+  console.log(options);
 });
 
 // ==================== Routers ========================
@@ -30,9 +31,43 @@ app.use(base, (req, res, next) => {
 
 app.use('/dataget', rest);
 
-rest.use('/site_info', (req, res) => {
-    res.json(sites);
-});
+function ssl(request, response) {
+    var optionsget = {
+        host : 'geapmuat2.run.aws-jp01-pr.ice.predix.io', // here only the domain name
+        // (no http/https !)
+        port : 443,
+        path : '/web/dataget/site_info', // the rest of the url with parameters if needed
+        method : 'GET' // do GET
+    };
+
+    var reqGet = https.request(optionsget, function(res) {
+        console.log("statusCode: ", res.statusCode);
+        // uncomment it for header details
+    //  console.log("headers: ", res.headers);
+    
+    
+        res.on('data', function(d) {
+            console.info('GET result:\n');
+            console.log(d.toString('utf8'));
+            response.json(JSON.parse(d.toString('utf8')));
+            // process.stdout.write(d);
+            console.info('\n\nCall completed');
+        });
+    
+    });
+    
+    reqGet.end();
+    reqGet.on('error', function(e) {
+        console.error(e);
+    });
+};
+
+rest.use('/site_info', ssl);
+// rest.use('/site_info', (req, res) => {
+//     // proxy.web(req, res, { target: `https://geapmuat2.run.aws-jp01-pr.ice.predix.io:443/web/dataget/site_info` });
+//     proxy.web(req, res, { target: `https://httpbin.org:443/get` });
+//     // res.json(sites);
+// });
 
 app.use('*', (req, res, next) => {
     next();
